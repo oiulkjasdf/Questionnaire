1cd3e9fb14517c6fade5370ba486b39fd7166e2c
Administrator <Administrator@UQNLQMGQ03O9KXY>
2017-11-07 17:22:52 +0800

    转载  https://github.com/yuanguangxin/Questionnaire
    本人学习借用

---
+++ src/com/controller/UserController.java
@@ -0,0 +1,72 @@
+package com.controller;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.models.Admin;
+import com.models.User;
+import com.service.AdminService;
+import com.service.UserService;
+import com.util.Decode;
+
+@Controller
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private AdminService adminService;
+
+    public void setAdminService(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/checkUsername")
+    public void checkUsername(@RequestParam("username") String username,
+                              HttpServletResponse response) throws Exception{
+        response.getWriter().print(userService.checkUsername(username));
+    }
+
+    @RequestMapping("/register")
+    public String register(User user, HttpSession session){
+        userService.register(user);
+        session.setAttribute("user",user);
+        return "login.html";
+    }
+
+    @RequestMapping("/loginCheck")
+    public void loginCheck(@RequestParam("username")String username,
+                             @RequestParam("password")String password,
+                             HttpServletResponse response,
+                             HttpSession session) throws Exception{
+        password = new String(Decode.decode(password));
+        User user = userService.login(username,password);
+        Admin admin = adminService.login(username,password);
+        if(user==null&&admin==null){
+            response.getWriter().print("wrong");
+        }else {
+            if(admin==null){
+                session.setAttribute("user",user);
+            }else {
+                session.setAttribute("admin",admin);
+            }
+        }
+    }
+
+    @RequestMapping("/userLogin")
+    public String login(HttpSession session){
+        if(session.getAttribute("admin")==null){
+            return "/getUserSurvey.action";
+        }else {
+            return "/admin.jsp";
+        }
+    }
+}
---
+++ src/com/service/SurveyService.java
@@ -0,0 +1,56 @@
+package com.service;
+
+import java.util.List;
+
+import com.mapper.SurveyMapper;
+import com.models.Survey;
+import com.models.SurveyExample;
+
+public class SurveyService {
+    private SurveyMapper surveyMapper;
+
+    public SurveyMapper getSurveyMapper() {
+        return surveyMapper;
+    }
+
+    public void setSurveyMapper(SurveyMapper surveyMapper) {
+        this.surveyMapper = surveyMapper;
+    }
+
+    public void createSurvey(Survey survey){
+        surveyMapper.updateByPrimaryKeySelective(survey);
+    }
+
+    public Survey getSurvey(int id){
+        return surveyMapper.selectByPrimaryKey(id);
+    }
+
+    public List<Survey> getAllSurvey(){
+        SurveyExample surveyExample = new SurveyExample();
+        SurveyExample.Criteria criteria = surveyExample.createCriteria();
+        criteria.andIdIsNotNull();
+        List<Survey> list = surveyMapper.selectByExample(surveyExample);
+        return list;
+    }
+
+    public void addSurvey(Survey survey){
+        surveyMapper.insert(survey);
+    }
+
+    public void deleteSurvey(int id){
+        surveyMapper.deleteByPrimaryKey(id);
+    }
+
+    public List<Survey> getUserSurvey(List<Integer> sid){
+        List<Survey> list = getAllSurvey();
+        for (int j=0;j<sid.size();j++){
+            for (int k=0;k<list.size();k++){
+                if(list.get(k).getId()==sid.get(j)){
+                    list.remove(k);
+                    break;
+                }
+            }
+        }
+        return list;
+    }
+}
